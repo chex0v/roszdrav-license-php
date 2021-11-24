377aaa0d9e194cfbc651db04a12e9f5571c377ed
chexov <chex0ff@inbox.ru>
2021-11-25 02:04:41 +0300

    feature new version packages

--- src/Service/LicenseClientService.php
+++ src/Service/LicenseClientService.php
@@ -3,10 +3,16 @@
 namespace Lh\RoszdravLicensePhp\Service;
 
 use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
+use Psr\Http\Message\ResponseInterface;
 
 class LicenseClientService implements IClientLicenseService
 {
     private static $instances = [];
+    /**
+     * @var Client
+     */
+    private $client;
 
     protected function __construct()
     {
@@ -33,24 +39,23 @@ class LicenseClientService implements IClientLicenseService
         return self::$instances[$cls];
     }
 
+    /**
+     * @throws \Exception
+     */
     public function __wakeup()
     {
         throw new \Exception("Cannot unserialize a singleton.");
     }
 
+    /**
+     * @throws GuzzleException
+     * @throws \Exception
+     */
     public function sendRequest(string $licenseNumber)
     {
         $params = $this->getBodyRequest();
         $params['q_no'] = $licenseNumber;
-        $response = $this->client->post('ajax/services/licenses', [
-            'form_params' => $params,
-            'headers' => [
-                'Content-Type' => 'application/x-www-form-urlencoded; charset=UTF-8',
-                'Accept' => 'application/json, text/javascript, */*; q=0.01',
-                'User-Agent' => 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36'
-            ]
-        ]);
-
+        $response = $this->send($params);
         if ($response->getStatusCode() !== 200) {
             throw new \Exception('Нет ответа от сервиса');
         }
@@ -94,6 +99,23 @@ class LicenseClientService implements IClientLicenseService
             'dt_to' => '',
             'q_org_label' => ''
         ];
+    }
+
+    /**
+     * @param array $params
+     * @return ResponseInterface
+     * @throws GuzzleException
+     */
+    private function send(array $params): ResponseInterface
+    {
+        return $this->client->post('ajax/services/licenses', [
+            'form_params' => $params,
+            'headers' => [
+                'Content-Type' => 'application/x-www-form-urlencoded; charset=UTF-8',
+                'Accept' => 'application/json, text/javascript, */*; q=0.01',
+                'User-Agent' => 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36'
+            ]
+        ]);
     }
 
     protected function __clone()
